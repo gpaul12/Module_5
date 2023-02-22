91e804f22fb0a35ae85faa3bec2922617cc99a2b
Gerry Paul <gpaul12@nc.rr.com>
2023-02-21 20:28:43 -0500

    next commit

--- 01-Activities/26-Stu_DayJS-Format/Unsolved/assets/js/script.js
+++ 01-Activities/26-Stu_DayJS-Format/Unsolved/assets/js/script.js
@@ -1,11 +1,13 @@
 // Use Day.js to format the date and assign to the declared variable.
-
 // TODO: 1. What is your graduation date in the following format: Jan 1, 1999?
-
+var gradDay = dayjs('Jul 13, 2023');
+$('#1a').text(gradDay.format('MMM D, YYYY'));
 // TODO: 2. What day of the week will 1/1/2027 be? (e.g. Is it "Monday"?)
-
+var dow = dayjs('1-1-2027');
+$('#2a').text(dow.format('dddd'))
 // TODO: 3. What is the current time in the format: hours:minutes:seconds
-
+var time = dayjs().format('hh:mm:ss');
+$('#3a').text(time);
 // TODO: 4. What is the current Unix timestamp?
 
 // TODO: 5. Parse the following Unix timestamp, 1318781876, and convert into any time/date format.
